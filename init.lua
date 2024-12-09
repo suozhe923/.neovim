c142d3e689189f1b8dedb377d49ffebadb033d25
suozhe923 <12312125@mail.sustech.edu.cn>
2024-12-09 23:27:43 +0800

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,9 @@
+require("core.options")
+require("core.keymap")
+require("core.lazy")
+--require("config.lazy")
+require("lazy").setup("plugins")
+require("conf.colorscheme")
+require("conf.cmp")
+
+
